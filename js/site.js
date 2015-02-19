df81402dde1e0073b053cc5297f4544d7a5a02c8
Tyler Fowle <tylerfowle@gmail.com>
2015-02-18 22:29:33 -0600

    update

--- js/site.js
+++ js/site.js
@@ -1,8 +1,16 @@
 //site.js
 
 $(document).ready(function(){
-	$('.sidebar nav a').on('click', function(){
+
+	$('.sidebar nav a').on('click', function(e){
+		e.preventDefault;
+
 		$('.sidebar nav a').removeClass('active');
 		$(this).addClass('active');
+
+		$('.page').hide();
+		var x = $(this).attr('href');
+		$(x).slideToggle();
 	});
+
 });
