28e04955e7fe5f1c41aff3f25e670b8e38d4a827
Tyler Fowle <tylerfowle@gmail.com>
2015-04-20 22:43:29 -0500

    updated to site, add structure

--- js/site.js
+++ js/site.js
@@ -1,16 +1,40 @@
 //site.js
+function remover() {
+  $('body').removeClass('home_page work_page social_page contact_page');
+}
 
 $(document).ready(function(){
 
-	$('.sidebar nav a').on('click', function(e){
-		e.preventDefault;
 
-		$('.sidebar nav a').removeClass('active');
-		$(this).addClass('active');
+  $('nav.main_nav a').on('click', function(e){
 
-		$('.page').hide();
-		var x = $(this).attr('href');
-		$(x).slideToggle();
-	});
+    if ( $(window).outerWidth() <= 640 ) {
 
-});
+      e.preventDefault;
+
+      if ( $('body').hasClass('nav_is_visible') ) {
+
+        remover();
+        var className = $(this).parent('li').attr('class');
+        $('body').addClass(className + '_page');
+        $('body').toggleClass('nav_is_visible');
+
+      }
+      else {
+
+        $('body').toggleClass('nav_is_visible');
+
+      }
+
+    }
+    else {
+
+      remover();
+      var className = $(this).parent('li').attr('class');
+      $('body').addClass(className + '_page');
+
+    }
+  });
+
+
+}); //end document
