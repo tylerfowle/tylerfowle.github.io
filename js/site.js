8c816b6ee8908cb09410583a7ab2089efc28d1e9
Tyler Fowle <tylerfowle@gmail.com>
2015-05-25 19:23:18 -0500

    updated index to landing page, add beta site

--- js/site.js
+++ js/site.js
@@ -9,19 +9,40 @@ $(window).load(function(){
 $(document).ready(function(){
 
 
-
-
-
-  var elementTop = $('.nav').offset().top;
   $(window).scroll(function() {
-    if( $(window).scrollTop() >= elementTop ) {
-      $('body').addClass('nav_fixed');
+    if( $(window).scrollTop() >= 400 ) {
+      $('body').addClass('sticky_logo');
     } else {
-      $('body').removeClass('nav_fixed');
+      $('body').removeClass('sticky_logo');
+    }
+
+    if( $(window).scrollTop() >= 100 ) {
+      $('body').addClass('fixed_header');
+    } else {
+      $('body').removeClass('fixed_header');
     }
   });
 
+  $('.skills a').on('click',function(e){
+    e.preventDefault();
+  });
 
+  $('.has-bg').each(function(){
+    $(this).find('img').hide();
+    var bgsrc = $(this).find('img').attr('src');
+    $(this).css('background', 'url("' + bgsrc + '") 50% 50% / cover no-repeat');
+  });
 
+  $('.has-bg-screen').each(function(){
+    $(this).find('img').hide();
+    var bgsrc = $(this).find('img').attr('src');
+    $(this).addClass('screen').css('background', 'url("' + bgsrc + '") 50% 50% / cover no-repeat');
+  });
+
+  $('.has-bg-collection li').each(function(){
+    $(this).find('img').hide();
+    var bgsrc = $(this).find('img').attr('src');
+    $(this).addClass('screen').css('background', 'url("' + bgsrc + '") 50% 50% / cover no-repeat');
+  });
 
 }); //end document ready
