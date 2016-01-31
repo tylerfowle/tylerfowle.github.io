b80c929f98498f23db9b935843632ad2a4c8598a
Tyler Fowle <tylerfowle@gmail.com>
2016-01-30 21:25:47 -0600

    new design

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,82 @@
-var gulp = require('gulp'),
-	browserify = require('browserify'),
-	browserSync = require('browser-sync'),
-	jshint = require('gulp-jshint'),
-	rename = require('gulp-rename'),
-	sass = require('gulp-ruby-sass'),
-	transform = require('vinyl-transform'),
-	uglify = require('gulp-uglify'),
-	util = require('gulp-util'),
-	autoprefixer = require('gulp-autoprefixer');
- 
+var gulp       = require('gulp'),
+  util         = require('gulp-util'),
+  browserSync  = require('browser-sync'),
+  jshint       = require('gulp-jshint'),
+  rename       = require('gulp-rename'),
+  sass         = require('gulp-ruby-sass'),
+  uglify       = require('gulp-uglify'),
+  fileinclude  = require('gulp-file-include'),
+  notify       = require('gulp-notify'),
+  autoprefixer = require('gulp-autoprefixer'),
+  include      = require('gulp-include');
 
+//sass
 gulp.task('sass', function() {
-	return gulp.src(['scss/*.scss', '!scss/_*.scss'])
-		.pipe(sass({
-			style: 'compressed',
-            "sourcemap=none": true // hack to allow auto-prefixer to work
-        }))
-		.pipe(autoprefixer({
-            browsers: ['> 5%', 'last 2 versions', 'Firefox >= 30', 'Opera >= 12', 'Safari >= 5', 'Explorer >= 10'],
-            cascade: false
-        }))
-		.on('error', function (err) { console.log(err.message); })
-		.pipe(gulp.dest('css'))
-		.pipe(browserSync.reload({stream: true}));
+  return sass('./scss/stylesheet.scss', { style: 'compact' })
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(autoprefixer('> 5%, last 2 versions', 'Firefox >= 30', 'Opera >= 12', 'Safari >= 5', 'Explorer >= 9'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream({}))
+    .pipe(notify({ message: "sass file: <%= file.relative %>"}));
 });
 
-
-gulp.task('lint', function() {
-	return gulp.src(['js/*.js', '!js/**/*.min.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
-});
-
-gulp.task('browserify', function () {
-  var browserified = transform(function(filename) {
-    var b = browserify(filename);
-    return b.bundle();
-  });
-
-  return gulp.src(['js/*.js', '!js/**/*.min.js'])
-    .pipe(browserified)
-    .pipe(uglify({
-    	preserveComments: function (node, comment) {
-				return comment.value.charAt(0) === '!';
-    	}
+//html file include 
+gulp.task('fileinclude', function() {
+  return gulp.src(['./html/**/*.html', '!./html**/_*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: './html/'
     }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('js/min'));
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream({}))
+    .pipe(notify({ message: "html include file: <%= file.relative %>"}));
 });
 
+//lint js files
+gulp.task('lint', function() {
+  return gulp.src(['./js/site.js', './js/components/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(notify({ message: "js lint file: <%= file.relative %>"}));
+});
+
+//compile site.js files
+gulp.task('include', ['lint'], function() {
+  return gulp.src(['./js/site.js'])
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(include())
+    .pipe(rename({suffix: '.inc'}))
+    .pipe(gulp.dest('./js'))
+    .pipe(notify({ message: "js include file: <%= file.relative %>"}));
+});
+
+//min site.js files
+gulp.task('min', ['include'], function() {
+  return gulp.src(['./js/site.inc.js'])
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(uglify())
+    .pipe(rename({basename: 'site.min'}))
+    .pipe(gulp.dest('./js'))
+    .pipe(notify({ message: "js min file: <%= file.relative %>"}));
+});
+
+//browser sync
 gulp.task('browser-sync', function() {
-	browserSync.init(null, {
-		server: { baseDir: "./", directory: true }
-	});
+  browserSync.init({
+    open: false,
+    notify: false,
+    server: {
+      baseDir: "./",
+      index: "html/index.html",
+      directory: true
+    }
+  });
 });
 
 gulp.task('default', ['browser-sync'], function() {
-	gulp.watch('scss/**/*.scss', ['sass']);
-	gulp.watch('**/*.html', browserSync.reload);
-	gulp.watch(['js/*.js'], ['lint', 'browserify', browserSync.reload]);
+  gulp.watch('scss/**/*.scss', ['sass']);
+  gulp.watch('html/**/*.html', ['fileinclude']);
+  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('js/**/*.js', ['min', browserSync.reload]);
 });
